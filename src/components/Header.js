be6052af47d6df95c651dd6dca54439708ccb3e1
roman alekseev <roma.alekseev.90@bk.ru>
2024-11-18 23:16:40 +0300

    Доработка шапки

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,30 @@ import logo from "../assets/img/logo.svg";
 import "./Header.css";
 
 const Header = () => {
+  const navItems = [
+    { name: "Home", link: "/" },
+    { name: "Episodes", link: "/episodes" },
+    { name: "About", link: "/about" },
+    { name: "Contact", link: "/contact" },
+  ];
+
   return (
     <header className="header">
       <a href="/" className="logo">
-        <img src={logo} alt="Castaway" className="logo-img"></img>
+        <img src={logo} alt="Castaway" className="logo-img" />
       </a>
-      <ul className="header-list">
-        <li className="header-item active">Home</li>
-        <li className="header-item">Episodes</li>
-        <li className="header-item">About</li>
-        <li className="header-item">Contact</li>
-      </ul>
+
+      <nav className="nav">
+        {navItems.map((item, index) => (
+          <a
+            key={index}
+            href={item.link}
+            className={`nav-link ${item.name === "Home" ? "active" : ""}`}
+          >
+            {item.name}
+          </a>
+        ))}
+      </nav>
     </header>
   );
 };
