ee2471cc7dfe3b1246f4bb383a1fc9fe2f45ea1f
roman alekseev <roma.alekseev.90@bk.ru>
2024-11-21 08:46:58 +0300

    переписан компонент EpisodesPage

--- src/components/Episodes/EpisodesPage.js
+++ src/components/Episodes/EpisodesPage.js
@@ -1,23 +1,59 @@
 import React from "react";
 import "./EpisodesPage.css";
 
+import cardImg1 from "../../assets/img/card-img-1.jpg";
+import cardImg2 from "../../assets/img/card-img-2.jpg";
+import cardImg3 from "../../assets/img/card-img-3.jpg";
+
 const EpisodesPage = () => {
   const episodes = [
-    { title: "Episode 1", description: "Description of Episode 1" },
-    { title: "Episode 2", description: "Description of Episode 2" },
+    {
+      img: cardImg1,
+      label: "Gear",
+      episode: "Episode 3",
+      title: "Should you get outboard audio gear?",
+      desc: "Is hardware really worth it when it comes to podcasting? The answer is...it depends. Here’s our reasons on why you might want to consider picking something up.",
+    },
+    {
+      img: cardImg2,
+      label: "Tips & Tricks",
+      episode: "Episode 2",
+      title: "Mic tricks to take you to the next level",
+      desc: "Stop rolling with those default settings on your mic. These small tweaks will take you from sounding good to great.",
+    },
+    {
+      img: cardImg3,
+      label: "Gear",
+      episode: "Episode 1",
+      title: "The best microphone under $200",
+      desc: "With so many microphones on the market, how are you supposed to know what’s the best? Take a look at our top picks.",
+    },
   ];
 
   return (
-    <div>
-      <h1>Episodes</h1>
-      <ul>
-        {episodes.map((episode, index) => (
-          <li key={index}>
-            <h2>{episode.title}</h2>
-            <p>{episode.description}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="episodes">
+      <header className="header-ep">
+        <h2 className="ep-title">Latest episodes</h2>
+        <a href="#" className="button">
+          View all episodes
+        </a>
+      </header>
+      {episodes.map((ep, index) => (
+        <div key={index} className="card-container">
+          <div className="card-image-block">
+            <img className="card-image" src={ep.img} alt="" />
+          </div>
+          <div className="card-text-block">
+            <span className="ep-label">{ep.label}</span>
+            <h3 className="card-title">{ep.episode}</h3>
+            <h2 className="card-title-strong">{ep.title}</h2>
+            <p className="card-desc">{ep.desc}</p>
+            <a href="#" className="button">
+              View Episode Details
+            </a>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
