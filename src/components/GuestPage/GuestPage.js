bf03d2f2c09ed462c905654380a47c44531dc1b1
roman alekseev <roma.alekseev.90@bk.ru>
2024-11-21 21:58:52 +0300

    Добавлена страница GuestPage

---
+++ src/components/GuestPage/GuestPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import arrowImg from "../../assets/img/arrow.png";
+import guestImg from "../../assets/img/guest-img.jpg";
+import "./GuestPage.css";
+
+const GuestPage = () => {
+  return (
+    <div className="guest-container">
+      <div className="guest-desc">
+        <button className="guest-btn">
+          <img src={arrowImg} alt="Button"></img>
+        </button>
+        <span className="button-text">Meet your host</span>
+        <h2 className="guest-name">Jacob Paulaner</h2>
+        <p className="guest-text">
+          Jacob has a background in audio engineering, and has been podcasting
+          since the early days.
+        </p>
+        <p className="guest-text">
+          He’s here to help you level up your game by sharing everything he’s
+          learned along the way.
+        </p>
+      </div>
+      <div className="guest-photo">
+        <img src={guestImg} alt="guest" className="guest-img"></img>
+      </div>
+    </div>
+  );
+};
+
+export default GuestPage;
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroPage from "../HeroPage/HeroPage";
 import EpisodesPage from "../Episodes/EpisodesPage";
+import GuestPage from "../GuestPage/GuestPage";
 
 const Homepage = () => {
   return (
     <div>
       <HeroPage />
       <EpisodesPage />
+      <GuestPage />
       {/* Добавьте другие секции */}
     </div>
   );
